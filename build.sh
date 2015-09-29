847ce8afa2fc36bfac6c00c4908063ae07c09cf6
bywwh <bywwh@mokeedev.com>
2015-09-30 05:58:58 +0800

    Add merge partition patch

--- build.sh
+++ build.sh
@@ -22,12 +22,35 @@ mv ./out/flyme*.zip ./out/flyme
 cd ./out/flyme
 unzip flyme*.zip
 rm -rf flyme*.zip
-cp -rf ../../other/updater-script.patch ./
-patch -p1 < updater-script.patch
-rm -rf updater-script.patch
+patch -p1 < ../../other/updater-script.patch
 cp -rf ../../other/supersu ./
 mkdir -p data/system_app
 mv system/app system/priv-app data/system_app
+
+# make merge partition patch
+mkdir -p flyme_merge_partition
+cp -rf boot.img system/framework/framework-res.apk ./flyme_merge_partition
+cd flyme_merge_partition
+../../../../../tools/apktool if framework-res.apk
+../../../../../tools/apktool d framework-res.apk
+unpack_bootimg boot.img boot.img.out
+patch -p1 < ../../../other/Merge_partition.patch
+../../../../../tools/apktool b framework-res
+pack_bootimg boot.img.out boot.img
+mv framework-res.apk framework-res.zip
+mv framework-res/build/apk/res ./
+zip -m framework-res.zip ./res/xml/storage_list.xml
+mv framework-res.zip framework-res.apk
+cp -rf ../../../other/META-INF ./
+mkdir -p system/framework
+mv framework-res.apk system/framework
+zip -r merge_partition.zip META-INF system boot.img
+java -jar ../../../../../build/tools/signapk.jar ../../../../../build/security/platform.x509.pem ../../../../../build/security/platform.pk8 merge_partition.zip flyme_merge_partition_$(date +%Y%m%d).zip
+mv flyme*.zip ../../
+cd ..
+rm -rf flyme_merge_partition
+# end
+
 zip -r flyme.zip *
 java -jar ../../../../build/tools/signapk.jar ../../../../build/security/platform.x509.pem ../../../../build/security/platform.pk8 flyme.zip flyme_aries_bywwh_$(date +%Y%m%d).zip
 mv flyme*.zip ../
