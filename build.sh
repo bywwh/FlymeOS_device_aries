d721043fc9b963bd223235c4eacf73104486f67c
bywwh <1065449148@qq.com>
2015-12-11 07:32:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,36 +21,32 @@ mv ./out/flyme*.zip ./out/flyme
 cd ./out/flyme
 unzip flyme*.zip
 rm -rf flyme*.zip
+rm -rf system/priv-app/Settings/Settings
 patch -p1 < ../../other/updater-script.patch
 cp -rf ../../other/supersu ./
 mkdir -p data/system_app
 mv system/app system/priv-app data/system_app
+zip -r flyme.zip *
+java -jar ../../../../build/tools/signapk.jar ../../../../build/security/platform.x509.pem ../../../../build/security/platform.pk8 flyme.zip FlymeOS_aries-$USER-$(date +%Y%m%d)-5.1.1.zip
+mv FlymeOS*.zip ../
 
 # make merge partition patch
-mkdir -p flyme_merge_partition
-cp -rf boot.img system/framework/framework-res.apk ./flyme_merge_partition
-cd flyme_merge_partition
+mv system/framework/framework-res.apk ./
 apktool if framework-res.apk
 apktool d framework-res.apk
 unpack_bootimg boot.img boot.img.out
-patch -p1 < ../../../other/Merge_partition.patch
+patch -p1 < ../../other/Merge_partition.patch
 apktool b framework-res
 pack_bootimg boot.img.out boot.img
 mv framework-res/build/apk/res ./
 zip -m framework-res.apk ./res/xml/storage_list.xml
-cp -rf ../../../other/META-INF ./
-mkdir -p system/framework
 mv framework-res.apk system/framework
-zip -r merge_partition.zip META-INF system boot.img
-java -jar ../../../../../build/tools/signapk.jar ../../../../../build/security/platform.x509.pem ../../../../../build/security/platform.pk8 merge_partition.zip FlymeOS_aries_merge_partition_$(date +%Y%m%d).zip
-mv FlymeOS*.zip ../../
-cd ..
-rm -rf flyme_merge_partition
+rm -rf framework-res res flyme.zip boot.img.out
+zip -r merge_partition.zip *
+java -jar ../../../../build/tools/signapk.jar ../../../../build/security/platform.x509.pem ../../../../build/security/platform.pk8 merge_partition.zip FlymeOS_aries-$USER-$(date +%Y%m%d)-mixed-5.1.1.zip
+mv FlymeOS*.zip ../
 # end
 
-zip -r flyme.zip *
-java -jar ../../../../build/tools/signapk.jar ../../../../build/security/platform.x509.pem ../../../../build/security/platform.pk8 flyme.zip FlymeOS_aries-$USER-$(date +%Y%m%d)-5.1.1.zip
-mv FlymeOS*.zip ../
 rm -rf ../flyme
 echo
 echo "刷机包编译完成！刷机包在out目录中"
