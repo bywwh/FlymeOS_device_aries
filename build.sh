86a6aee27113b24763641b677d4ecf6e170931b1
bywwh <1065449148@qq.com>
2015-12-14 20:05:52 +0800

    Set device

--- build.sh
+++ build.sh
@@ -1,61 +1,36 @@
 #!/bin/bash
+FLYME_OUT_DIR=/home/bywwh/AndroidSource/FlymeOS/devices/aries/out
+DEVICE_DIR=/home/bywwh/AndroidSource/FlymeOS/devices/aries
+OTHER_DIR=/home/bywwh/AndroidSource/FlymeOS/devices/aries/other
 cd ../..
-echo
-echo ">>> 初始化编译环境"
 . build/envsetup.sh
 echo
-echo
-echo ">>> 进入机型目录"
+echo ">>> in devices dir"
 cd -
 echo
-echo
-echo ">>> 清除缓存文件"
 flyme clean
-echo ">>> 编译刷机包"
 flyme fullota
 
-if [ -e ./out/flyme*.zip ]
-   then
-mkdir -p out/flyme
-mv ./out/flyme*.zip ./out/flyme
-cd ./out/flyme
-unzip flyme*.zip
-rm -rf flyme*.zip
-patch -p1 < ../../other/updater-script.patch
-cp -rf ../../other/supersu ./
-mkdir -p data/system_app
-mv system/app system/priv-app data/system_app
-cp -rf ../../other/data ./
+if [ -e $FLYME_OUT_DIR/flyme*.zip ];then
+mv -vf $FLYME_OUT_DIR/flyme*.zip $FLYME_OUT_DIR/FlymeOS_aries-$USER-$(date +%Y%m%d)-5.1.1.zip
+fi
 
-# make merge partition patch
-mkdir -p flyme_merge_partition
-cp -rf boot.img system/framework/framework-res.apk ./flyme_merge_partition
-cd flyme_merge_partition
-apktool if framework-res.apk
-apktool d framework-res.apk
-unpack_bootimg boot.img boot.img.out
-patch -p1 < ../../../other/Merge_partition.patch
-apktool b framework-res
-pack_bootimg boot.img.out boot.img
-mv framework-res/build/apk/res ./
-zip -m framework-res.apk ./res/xml/storage_list.xml
-cp -rf ../../../other/META-INF ./
-mkdir -p system/framework
-mv framework-res.apk system/framework
-zip -r merge_partition.zip META-INF system boot.img
-java -jar ../../../../../build/tools/signapk.jar ../../../../../build/security/platform.x509.pem ../../../../../build/security/platform.pk8 merge_partition.zip FlymeOS_aries_merge_partition_$(date +%Y%m%d).zip
-mv FlymeOS*.zip ../../
-cd ..
-rm -rf flyme_merge_partition
-# end
+echo
+echo ">>> make merge partition patch"
+echo
+cp -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/boot.img.out.bak
+cp -rf $DEVICE_DIR/framework-res $DEVICE_DIR/framework-res.bak
+cp -rf $OTHER_DIR/storage_list.xml $DEVICE_DIR/framework-res/res/xml
+cp -rf $OTHER_DIR/fstab.qcom $OTHER_DIR/init.target.rc $DEVICE_DIR/boot.img.out/RAMDISK
+echo
+echo ">>> make fullota again"
+echo
+cd $DEVICE_DIR
+flyme fullota
+rm -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/framework-res
+mv -vf $DEVICE_DIR/boot.img.out.bak $DEVICE_DIR/boot.img.out
+mv -vf $DEVICE_DIR/framework-res.bak $DEVICE_DIR/framework-res
 
-zip -r flyme.zip *
-java -jar ../../../../build/tools/signapk.jar ../../../../build/security/platform.x509.pem ../../../../build/security/platform.pk8 flyme.zip FlymeOS_aries-$USER-$(date +%Y%m%d)-5.1.1.zip
-mv FlymeOS*.zip ../
-rm -rf ../flyme
-echo
-echo "刷机包编译完成！刷机包在out目录中"
-cd ..
-pwd
-echo
+if [ -e $FLYME_OUT_DIR/flyme*.zip ];then
+mv -vf $FLYME_OUT_DIR/flyme*.zip $FLYME_OUT_DIR/FlymeOS_aries-$USER-$(date +%Y%m%d)-mixed-5.1.1.zip
 fi
---
+++ custom_targetfiles.sh
@@ -0,0 +1,22 @@
+echo ">>> in $0"
+OUT_DIR=/home/bywwh/AndroidSource/FlymeOS/devices/aries/out/merged_target_files
+
+cd $OUT_DIR
+echo
+echo ">>> delete $OUT_DIR/DATA"
+echo
+rm -rf DATA/
+echo
+echo ">>> delete $OUT_DIR/SYSTEM/priv-app/Settings/Settings"
+echo
+rm -rf $OUT_DIR/SYSTEM/priv-app/Settings/Settings
+echo
+echo ">>> create dir DATA/"
+echo
+mkdir -vp DATA/
+echo
+echo ">>> move some apps into data"
+echo
+mkdir -vp $OUT_DIR/DATA/system_app
+mv -vf $OUT_DIR/SYSTEM/app $OUT_DIR/DATA/system_app
+mv -vf $OUT_DIR/SYSTEM/priv-app $OUT_DIR/DATA/system_app
