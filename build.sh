db9a72ccea87057d95d42da06037194ed1000fcb
bywwh <1065449148@qq.com>
2015-12-16 08:04:53 +0800

    Update

--- build.sh
+++ build.sh
@@ -18,19 +18,27 @@ fi
 echo
 echo ">>> make merge partition patch"
 echo
-cp -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/boot.img.out.bak
-cp -rf $DEVICE_DIR/framework-res $DEVICE_DIR/framework-res.bak
-cp -rf $OTHER_DIR/storage_list.xml $DEVICE_DIR/framework-res/res/xml
-cp -rf $OTHER_DIR/fstab.qcom $OTHER_DIR/init.qcom.rc $DEVICE_DIR/boot.img.out/RAMDISK
+cp -v -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/boot.img.out.bak
+cp -v -rf $DEVICE_DIR/framework-res $DEVICE_DIR/framework-res.bak
+cp -v -rf $OTHER_DIR/storage_list.xml $DEVICE_DIR/framework-res/res/xml
+cp -v -rf $OTHER_DIR/fstab.qcom $OTHER_DIR/init.qcom.rc $DEVICE_DIR/boot.img.out/RAMDISK
 echo
 echo ">>> make fullota again"
 echo
+mkdir -vp $DEVICE_DIR/tmp
+mv -vf $FLYME_OUT_DIR/*.zip $DEVICE_DIR/tmp
 cd $DEVICE_DIR
+flyme clean
 flyme fullota
-rm -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/framework-res
+rm -v -rf $DEVICE_DIR/boot.img.out $DEVICE_DIR/framework-res
 mv -vf $DEVICE_DIR/boot.img.out.bak $DEVICE_DIR/boot.img.out
 mv -vf $DEVICE_DIR/framework-res.bak $DEVICE_DIR/framework-res
 
 if [ -e $FLYME_OUT_DIR/flyme*.zip ];then
 mv -vf $FLYME_OUT_DIR/flyme*.zip $FLYME_OUT_DIR/FlymeOS_aries-$USER-$(date +%Y%m%d)-mixed-5.1.1.zip
+fi
+
+if [ -e $DEVICE_DIR/tmp/Flyme*.zip ] || [ -e $DEVICE_DIR/tmp/target*.zip ];then
+mv -vf $DEVICE_DIR/tmp/*.zip $FLYME_OUT_DIR
+rm -v -rf $DEVICE_DIR/tmp
 fi
--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -5,11 +5,11 @@ cd $OUT_DIR
 echo
 echo ">>> delete $OUT_DIR/DATA"
 echo
-rm -rf DATA/
+rm -v -rf DATA/
 echo
 echo ">>> delete $OUT_DIR/SYSTEM/priv-app/Settings/Settings"
 echo
-rm -rf $OUT_DIR/SYSTEM/priv-app/Settings/Settings
+rm -v -rf $OUT_DIR/SYSTEM/priv-app/Settings/Settings
 echo
 echo ">>> create dir DATA/"
 echo
